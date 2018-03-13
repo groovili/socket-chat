1a71ec4166493123e367553d7365ac3fbcd9ef37
groovili <maxim.ivanov@panenco.com>
2018-03-13 11:09:28 +0200

    Profiler recipe

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,5 @@ return [
     OldSound\RabbitMqBundle\OldSoundRabbitMqBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
