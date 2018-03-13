a8fd9347510b5f82b63ec9ba2551f497ac862a90
groovili <m.ivanov.work@gmail.com>
2018-03-13 20:27:22 +0200

    Readme, small ux improvements

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,10 @@
 var socket = new WebSocket("ws://127.0.0.1:8080");
 
-socket.onopen = function() {
+socket.onopen = function () {
     console.log('Connection successful');
 };
 
-socket.onclose = function(event) {
+socket.onclose = function (event) {
     if (event.wasClean) {
         console.log('Connection closed.');
     } else {
@@ -13,22 +13,41 @@ socket.onclose = function(event) {
     console.log(event.code + event.reason);
 };
 
-socket.onmessage = function(event) {
+socket.onmessage = function (event) {
     var list = document.getElementById('list');
-    var p = document.createElement("p");
+    var message = document.createElement("div");
     var node = document.createTextNode(event.data);
-    p.appendChild(node);
-    list.appendChild(p);
+    message.appendChild(node);
+    message.classList.add('card');
+    list.appendChild(message);
 };
 
-socket.onerror = function(error) {
+socket.onerror = function (error) {
     console.log(error.message);
 };
 
 var button = document.getElementById('send');
-button.onclick = function (ev) {
-    var text = document.getElementById('message-box').value;
-    if(text.length > 0){
+var textarea = document.getElementById('message-box');
+
+function sendText() {
+    var text = textarea.value;
+    if (text.length > 0) {
         socket.send(JSON.stringify(text));
+        textarea.value = '';
+
+        return true;
     }
+
+    return false;
 }
+
+button.onclick = sendText;
+
+textarea.onkeypress = function (ev) {
+    if (ev.charCode === 13 && ev.shiftKey) {
+        sendText();
+    }
+};
+
+
+
